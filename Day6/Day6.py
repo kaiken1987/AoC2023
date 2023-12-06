5b677e6bc59ce4c2a453e3df56679a98663ee2e7
Ken <Ken>
2023-12-06 17:26:25 -0500

    Day 6 part 1

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,12 +1,33 @@
+from ctypes import BigEndianStructure
 
-f = open("example.txt", "r")
+
+class race:
+	time = 0
+	best = 0
+	def __init__(self, t,d) -> None:
+		self.time = t
+		self.best = d	
+
+#races = [race(7,9),race(15,40),race(30,200)]
+races = [race(44,208),race(80,1581),race(65,1050),race(72,1102)]
+		
+#f = open("example.txt", "r")
 #f = open("input.txt", "r")
 
 def part1():
-   print( "Part 1")
-   
+	print( "Part 1")
+	product = 1	
+	for r in races:	
+		numways = 0		
+		for i in range(1,r.time):
+			if((r.time-i)*i>r.best):
+				numways = r.time-i*2+1 				
+				break			
+		print(f"Race has {numways} ways to win")			
+		product *= numways
+	print(f"Answer is {product}")					
 def part2():
-   print( "Part 2")
+	print( "Part 2")
    
 part1()
 part2()
