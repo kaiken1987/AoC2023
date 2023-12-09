5c91724a5c2df076f9a5de63c0ff3a6fd982d2af
Ken <Ken>
2023-12-09 09:59:10 -0500

    Day 9 part 1 and day 8 1&2

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,12 +1,73 @@
+import primefac 
 
-f = open("example.txt", "r")
-#f = open("input.txt", "r")
+
+#f = open("example.txt", "r")
+f = open("input.txt", "r")
 
 def part1():
    print( "Part 1")
+   route = f.readline()
+   f.readline()   
+   network = {}
+   for line in f:
+      start = line[0:3]               
+      left = line[7:10]               
+      right = line[12:15]
+      network[start] = (left,right)
+   pos = 'AAA'
+   idx = 0   
+   steps = 0   
+   while(pos!='ZZZ'):   
+      if(route[idx]=='\n'):
+         idx = 0         
+      if(route[idx]=='L'):
+         pos = network[pos][0]
+      else:
+         pos = network[pos][1]
+      idx = idx+1
+      steps += 1
+   print(f"A journey of {steps} steps")            
+         
+def testDone( pos : list)->bool:
+   for p in pos:
+      if(p[-1] != 'Z'):
+         return False
+   return True
    
 def part2():
    print( "Part 2")
+   route = f.readline()
+   f.readline()   
+   network = {}
+   pos = []
+   for line in f:
+      start = line[0:3]
+      if( start[2] == 'A'):
+         pos.append(start)
+      left = line[7:10]               
+      right = line[12:15]
+      network[start] = (left,right)
+   cycles =[]   
+             
+   for p in pos:   
+      idx = 0   
+      steps = 0 
+      while(p[-1] != 'Z'):      
+         if(route[idx]=='\n'):
+            idx = 0            
+         if(route[idx]=='L'):
+            p = network[p][0]
+         else:
+            p = network[p][1]
+         idx = idx+1
+         steps += 1
+      cycles.append(steps)   
+   steps = 1            
+   for c in cycles:
+      fac = list(primefac.primefac( c) )
+      print(f"{c}: {fac}")      
+      steps *= fac[0]
+   print(f"A journey of {steps} steps") 
    
-part1()
+#part1()
 part2()
--- Day9/Day9.py
+++ Day9/Day9.py
@@ -1,9 +1,33 @@
 
-f = open("example.txt", "r")
-#f = open("input.txt", "r")
+#f = open("example.txt", "r")
+f = open("input.txt", "r")
+def createHistory(values:list)->list:
+   history = [values]
+   while(history[-1].count(0)!=len(history[-1])):
+      curr = []     
+      for i in range(1,len(values)):
+         curr.append(values[i]-values[i-1])
+      history.append(curr)
+      values = curr      
+   history.reverse()
+   return history
+   
+def predictNext( values :list )->int:
+   history = createHistory(values)   
+   pred = 0         
+   for h in history:
+      pred += h[-1]   
+   return pred                        
 
 def part1():
    print( "Part 1")
+   sum = 0   
+   for line in f:
+      values = [int(x) for x in line.strip().split() ]
+      next = predictNext( values )
+      sum += next
+   print(f"Sum of predictions {sum}")            
+                      
    
 def part2():
    print( "Part 2")
